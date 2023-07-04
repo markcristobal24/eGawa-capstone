3ceadc286741c29531ea93f7f30c181e2c2f22c2
arvinbok <bokarvin1904@gmail.com>
2023-07-04 15:45:52 +0800

    added id verification page

--- js/freelance.js
+++ js/freelance.js
@@ -41,3 +41,23 @@ function loadImage(event) {
     }
     reader.readAsDataURL(event.target.files[0]);
 }
+
+
+//for id1 verification of freelancer
+function loadImage1(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var uploadedImage = document.getElementById('uploadedImage1');
+        uploadedImage.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
+//for id2 verification of freelancer
+function loadImage2(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var uploadedImage = document.getElementById('uploadedImage2');
+        uploadedImage.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
