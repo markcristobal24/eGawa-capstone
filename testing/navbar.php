6f30baed53b321430a985a987ceafb571154c302
Mark Cristobal <markjoshcristobal24@gmail.com>
2023-07-29 12:33:47 +0800

    testing navbar and footer

---
+++ testing/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-md navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="../img/eGAWAwhite.png" alt="Logo" id="logoImage"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a id="home1" class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a id="about1" id="about" class="nav-link" href="../aboutUs.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a id="freeLanceInbox" class="nav-link" href="freeLanceInbox.php">Messages</a>
+                </li>
+                <!-- <li class="nav-item dropdown">
+                    <a id="freelanceOption" class="nav-link" href="#">Welcome,
+                        <span>
+                            <?php echo $_SESSION['firstName']; ?>
+                        </span></a>
+                    <div class="dropdown-content">
+                        <a href="freelanceChangeEmail.php">Change Email Address</a>
+                        <a href="freelanceChangePass.php">Change Password</a>
+                        <a id="logout1" href="#">Log Out</a>
+                    </div>
+                </li> -->
+            </ul>
+        </div>
+    </div>
+</nav>
