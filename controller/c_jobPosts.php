f86029238f070955a959fe0fa84911cdeb6d37a4
Mark Cristobal <markjoshcristobal24@gmail.com>
2023-08-09 19:35:03 +0800

    update

--- controller/c_jobPosts.php
+++ controller/c_jobPosts.php
@@ -1,7 +1,23 @@
 <?php
-require_once dirname(__FILE__) . "/../php/classes/DbClass.php";
+require_once dirname(__FILE__) . "/../php/classes/DbConnection.php";
 
 if (isset($_POST['jobPosts'])) {
-    
+    $user_id = $_SESSION['account_id'];
+
+    $post_category = $_POST['post_category'];
+    $post_title = $_POST['post_title'];
+    $post_description = $_POST['post_description'];
+    $post_tags = $_POST['post_tags'];
+    $rate = $_POST['rate'];
+
+    $currentDateTime = date("Y-m-d H:i:s");
+    $dateTimeObj = new DateTime($currentDateTime);
+    $posted_date = $dateTimeObj->format("F d, Y h:i A");
+
+    if ($post_title === "" || $post_description === "" || $post_tags === "" || $rate === "") {
+        $output['error'] = "Incomplete Details!";
+    } else {
+
+    }
 }
 ?>
