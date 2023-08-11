904c904a4cf67dd181fcdfd7ddbe463dd3da6ac3
Mark Cristobal <markjoshcristobal24@gmail.com>
2023-08-11 13:53:02 +0800

    fixed user registration

--- classJS/Account.js
+++ classJS/Account.js
@@ -294,6 +294,35 @@ class Account {
         });
     }
 
+    user_register() {
+        let button_value = new Account().get_button_value("btnUserReg");
+        new Account().button_loading("btnUserReg", "loading", "");
+
+        var form_data = new FormData(document.getElementById('account_form'));
+        form_data.append('user_register', 'user_register');
+        fetch('../controller/c_uRegister.php', {
+            method: "POST",
+            body: form_data
+        }).then(function (response) {
+            return response.json();
+        }).then(function (response_data) {
+            console.log(response_data);
+            if (response_data.success) {
+                console.log(response_data.success);
+                new Notification().create_notification(response_data.success, "success");
+                let tID = setTimeout(function () {
+                    window.location.replace('../freelance/verifyAccount.php');
+                    window.clearTimeout(tID);
+                }, 3000);
+            }
+            else if (response_data.error) {
+                console.log(response_data.error);
+                new Account().button_loading("btnUserReg", "", button_value);
+                new Notification().create_notification(response_data.error, "error");
+            }
+        });
+    }
+
     button_loading(element, type, text) {
         if (type == "loading") {
             document.getElementById(element).innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
