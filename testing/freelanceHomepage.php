0757df485a9ed43f724135b65fb7f11908832b4e
vinnny1904 <bokarvin1904@gmail.com>
2023-08-27 16:16:58 +0800

    modifying

--- testing/freelanceHomepage.php
+++ testing/freelanceHomepage.php
@@ -180,7 +180,7 @@ $fetch = $sql->fetch_all();
                 <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Catalog</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body-add">
                 <div class="input">
                     <img id="uploadedImageCatalog" src="../img/uploadIMG.png" alt="Uploaded Image" height="200">
                     <input id="uploadInput" type="file" name="catalogImg" accept="image/*"
