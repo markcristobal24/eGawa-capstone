856768ec7f711020709e0e4e87dc95d9ffb80994
vinnny1904 <bokarvin1904@gmail.com>
2023-09-10 15:54:03 +0800

    modified

--- freelance/freelance_message.php
+++ freelance/freelance_message.php
@@ -130,7 +130,13 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
                                     Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic
                                     or web designs. The passage is attributed to an unknown typesetter in the 15th century who is
                                     thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type
-                                    specimen book. It usually begins wit
+                                    specimen book. It usually begins witwidth: fit-content;
+
+                                    Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic
+                                    or web designs. The passage is attributed to an unknown typesetter in the 15th century who is
+                                    thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type
+                                    specimen book. It usually begins witwidth: fit-content;
+
                                 </span>
                             </div>
                             <div class="freelance-chat">
