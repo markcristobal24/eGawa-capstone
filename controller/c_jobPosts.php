3cdd6197ced9579dee6ab0945f6e822a599029e6
vinnny1904 <bokarvin1904.com>
2023-09-20 16:04:39 +0800

    modified (tried to fix the font capslock)

--- controller/c_jobPosts.php
+++ controller/c_jobPosts.php
@@ -96,22 +96,28 @@ if (isset($_POST['filter_post'])) {
             $posted_date = $dateTimeObj->format("F d, Y h:i A");
             $output['success'] .= '
             <div class="containerPost">
-                <span class="titlePost">' . $row['post_title'] . '</span>
-                <div>
-                    <span class="author">Author: </span>
-                    <span class="userPost">' . $row['firstName'] . ' ' . $row['lastName'] . '</span>
-                </div>
-                <div>
-                    <span class="locationPost">' . $row['address'] . '</span>
-                    <span>•</span>
-                    <span class="datePost">Posted on ' . $posted_date . '</span>
-                </div>
 
-                <p class="descPost">
-                    ' . $row['post_description'] . '
-                </p>
-                <div>
-                    <button id="viewPostBTN" data-bs-toggle="modal" data-bs-target="#view-post-modal" onclick="new Posts().view_post(' . $row['post_id'] . ');">View Post</button>
+                <div class="post-col-1">
+                    <img class="author-pic" src="../img/profile.png" alt="" ">
+                </div>
+                <div class="post-col-2">
+                    <span class="titlePost">' . $row['post_title'] . '</span>
+                    <div>
+                        <span class="author">Author: </span>
+                        <span class="userPost">' . $row['firstName'] . ' ' . $row['lastName'] . '</span>
+                    </div>
+                    <div>
+                        <span class="locationPost">' . $row['address'] . '</span>
+                        <span>•</span>
+                        <span class="datePost">Posted on ' . $posted_date . '</span>
+                    </div>
+
+                    <p class="descPost">
+                        ' . $row['post_description'] . '
+                    </p>
+                    <div>
+                        <button id="viewPostBTN" data-bs-toggle="modal" data-bs-target="#view-post-modal" onclick="new Posts().view_post(' . $row['post_id'] . ');">View Post</button>
+                    </div>
                 </div>
             </div>
             ';
