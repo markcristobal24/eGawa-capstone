86c6349504d9ad8ca7f3b51a57b1f58c33d3918f
vinnny1904 <bokarvin1904.com>
2023-09-30 14:14:24 +0800

    added admin page (WIP)

---
+++ admin.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Link for Bootstrap 5 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+
+    <!-- Link for CSS -->
+    <link rel="stylesheet" href="admin.css" />
+    <link rel="stylesheet" href="css/notification.css">
+
+    <!-- For social icons in the footer -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
+
+    <title>eGawa | Admin</title>
+</head>
+<body>
+    <header>
+        <h1>eGawa</h1>
+    </header>
+    <?php 
+        // include "../other/navbar.php"; 
+    ?>
+    
+    <div class="container-">
+        <nav class="sidebar">
+            <ul>
+                <li><a href="#" class="list-">Sample</a></li>
+                <li><a href="#" class="list-">Sample</a></li>
+                <li><a href="#" class="list-">Sample</a></li>
+                <li><a href="#" class="list-">Sample</a></li>
+            </ul>
+        </nav>
+
+        <div class="content">
+            <h2>Main Content</h2>
+            <div class="box-">
+                <div class="box-1 boxes">
+                    <span>Total Users:</span>
+                    <span class="boxes-data">100</span>
+                </div>
+                <div class="box-2 boxes">
+                    <span>Revenue:</span>
+                    <span class="boxes-data">100,000.00</span>
+                </div>
+                <div class="box-3 boxes"></div>
+                <div class="box-4 boxes"></div>
+                <div class="box-5 boxes"></div>
+                <!-- <div class="box-1 boxes"></div>
+                <div class="box-2 boxes"></div>
+                <div class="box-3 boxes"></div>
+                <div class="box-4 boxes"></div>
+                <div class="box-5 boxes"></div>
+                <div class="box-1 boxes"></div>
+                <div class="box-2 boxes"></div>
+                <div class="box-3 boxes"></div>
+                <div class="box-4 boxes"></div>
+                <div class="box-5 boxes"></div>
+                <div class="box-1 boxes"></div>
+                <div class="box-2 boxes"></div>
+                <div class="box-3 boxes"></div>
+                <div class="box-4 boxes"></div>
+                <div class="box-5 boxes"></div> -->
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.js"
+        integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
