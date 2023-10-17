a2c3a3f506645e531c6ff9286606d261bf2dcd91
Mark Josh Adrian Cristobal <markjoshcristobal24@gmail.com>
2023-10-17 13:23:35 +0800

    removed vendor and other dead codes

--- testing/test.php
+++
@@ -1,8 +0,0 @@
-<div class="modal" id="myModal">
-    <div class="modal-content">
-      <span class="close" id="closeModalBtn">&times;</span>
-      <h2>Modal Window</h2>
-      <p>This is a sample modal window.</p>
-    </div>
-    <button id="closeme">close</button>
-</div>
