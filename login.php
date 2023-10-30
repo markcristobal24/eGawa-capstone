c5afede94bb1274acaf9db4f5c103cb3abd74168
Mark Cristobal <markjoshcristobal24@gmail.com>
2023-10-30 12:56:33 +0800

    .

--- login.php
+++ login.php
@@ -124,7 +124,7 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.7.0.js"
         integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
 
-    <script src="js/script.js"></script>
+
     <script src="classJS/Account.js"></script>
     <script src="classJS/Notification.js"></script>
     <script src="js/validate.js"></script>
