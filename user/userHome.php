098b316abe9f09986ab12f285708a92870b0d13a
vinnny1904 <bokarvin1904.com>
2023-10-31 12:23:42 +0800

    moved edit/delete job post in company profile

--- user/userHome.php
+++ user/userHome.php
@@ -31,9 +31,7 @@ $fetch = $query->fetch(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="../css/notification.css">
 
     <!-- For social icons in the footer -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="shortcut icon" href="../img/egawaicon4.png" type="image/x-icon">
     <title>eGawa | User Home</title>
@@ -56,8 +54,7 @@ $fetch = $query->fetch(PDO::FETCH_ASSOC);
                 <div class="left-nav-dropdown">
                     <div class="dropdownOption">
                         <form id="filterpost_form" method="POST">
-                            <select id="filterOption" name="filterOption"
-                                onchange="new Posts().filter_post(this.value);">
+                            <select id="filterOption" name="filterOption" onchange="new Posts().filter_post(this.value);">
                                 <option value="all">All</option>
                                 <option value="Website Development">Website Development</option>
                                 <option value="Mobile Development">Mobile Development</option>
@@ -69,9 +66,7 @@ $fetch = $query->fetch(PDO::FETCH_ASSOC);
                 </div>
                 <div class="left-nav-search">
                     <form class="d-flex">
-                        <input class="form-control me-2 search" type="text" id="search_post"
-                            onkeyup="new Posts().search_post(this.value);" placeholder="Search a tag"
-                            aria-label="Search">
+                        <input class="form-control me-2 search" type="text" id="search_post" onkeyup="new Posts().search_post(this.value);" placeholder="Search a tag" aria-label="Search">
                         <!-- <button class="btn btn-success" type="submit">Search</button> -->
                     </form>
                 </div>
@@ -141,12 +136,9 @@ $fetch = $query->fetch(PDO::FETCH_ASSOC);
                                         if (isset($_SESSION['user_image'])) {
                                         ?> src="../img/uploads/company/<?php echo $_SESSION['user_image']; ?>" <?php
                                                                                                             } else {
-                                                                                                                ?>
-                        src="../img/profile.png"
-                        <?php
-                                                                                                                        }
-                                                                                                                            ?>
-                        alt="user profile" title="user profile">
+                                                                                                                ?> src="../img/profile.png" <?php
+                                                                                                                                        }
+                                                                                                                                            ?> alt="user profile" title="user profile">
                     <p id="userName">
                         <?php echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName']; ?>
                     </p>
@@ -186,14 +178,12 @@ $fetch = $query->fetch(PDO::FETCH_ASSOC);
                         <div class="rateInput input-group mb-3 mt-2">
 
                             <span class="input-group-text">&#8369;</span>
-                            <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)"
-                                name="rate" placeholder="Enter rate" required>
+                            <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)" name="rate" placeholder="Enter rate" required>
                             <span class="input-group-text">.00</span>
                         </div>
 
                         <div class="btns">
-                            <input id="submitPost" class="btn" type="button" value="Submit"
-                                onclick="new Posts().post();">
+                            <input id="submitPost" class="btn" type="button" value="Submit" onclick="new Posts().post();">
                             <input id="clearPost" class="btn" type="button" value="Clear">
                         </div>
                     </form>
@@ -311,98 +301,22 @@ $fetch = $query->fetch(PDO::FETCH_ASSOC);
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#edit-post-modal">Edit</button>
+                    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-post-modal">Edit</button> -->
                     <!-- <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button> -->
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- MODAL FOR EDIT POST MODAL-->
-    <div class="modal fade" id="edit-post-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-
-        <div class="modal-dialog modal-md modal-dialog-centered modal-dialog-scrollable">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Post</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form id="post_form" method="POST">
-                        <div class="toFlex">
-                            <div class="dropdownOptionPost">
-                                <select id="filterOptionPost" name="post_category">
-                                    <option value="Website Development">Website Development</option>
-                                    <option value="Mobile Development">Mobile Development</option>
-                                    <option value="Website Hosting">Website Hosting</option>
-                                    <option value="Multimedia">Multimedia</option>
-                                </select>
-                            </div>
-                        </div>
-                        
-                        <div class="mx-3">
-                            <input type="text"  id="title" name="post_title" placeholder="Job Title" required>   
-                        </div>
-
-                        <div class="descContainer mx-3">
-                            <textarea id="description" placeholder="Job Description" name="post_description"></textarea>
-                        </div>
-
-                        <div class="mx-3">
-                            <input type="text" id="tags" name="post_tags" placeholder="Tags" required>
-                        </div>
-
-                        <div class="rateInput input-group col-4">
-                            <span class="input-group-text">&#8369;</span>
-                            <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)"
-                                name="rate" placeholder="Enter rate" required>
-                            <span class="input-group-text">.00</span>
-                        </div>
-
-                        <!-- <div class="btns">
-                            <input id="submitPost" class="btn" type="button" value="Submit"
-                                onclick="new Posts().post();">
-                            <input id="clearPost" class="btn" type="button" value="Clear">
-                        </div> -->
-                </form>
-                
-                <div class="modal-footer">
-                    <!-- <button type="button" class="btn btn-danger"  data-bs-target="#delete-post-modal">Delete</button> -->
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-post-modal">Delete</button>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#edit-post-modal">Save</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- CONFIRMATION DELETE POST MODAL -->
-    <div class="modal fade" id="delete-post-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" >
-            <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Post?</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Are you sure you want to delete this post?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Back</button>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button>
-            </div>
-            </div>
-        </div>
-    </div>
 
     <script>
-    // JavaScript to make the textarea auto-resize
-    const textarea = document.getElementById('description');
+        // JavaScript to make the textarea auto-resize
+        const textarea = document.getElementById('description');
 
-    textarea.addEventListener('input', () => {
-        textarea.style.height = 'auto'; // Reset height to auto
-        textarea.style.height = textarea.scrollHeight + 'px'; // Set height to scrollHeight
-    });
+        textarea.addEventListener('input', () => {
+            textarea.style.height = 'auto'; // Reset height to auto
+            textarea.style.height = textarea.scrollHeight + 'px'; // Set height to scrollHeight
+        });
     </script>
 
 
@@ -413,8 +327,7 @@ $fetch = $query->fetch(PDO::FETCH_ASSOC);
     <script src="../classJS/Notification.js"></script>
     <script src="../classJS/Posts.js"></script>
 
-    <script src="https://code.jquery.com/jquery-3.7.0.js"
-        integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
--- user/userHomePage.php
+++ user/userHomePage.php
@@ -31,9 +31,7 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
     <link rel="stylesheet" href="../css/notification.css">
 
     <!-- For social icons in the footer -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="shortcut icon" href="../img/egawaicon4.png" type="image/x-icon">
     <title>eGawa | User Homepage</title>
@@ -90,8 +88,7 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
         </div>
 
         <div class="div1">
-            <img id="userPic" src="../img/uploads/company/<?php echo $_SESSION['user_image']; ?>" alt="user profile"
-                title="user profile">
+            <img id="userPic" src="../img/uploads/company/<?php echo $_SESSION['user_image']; ?>" alt="user profile" title="user profile">
             <h2 id="userName">
                 <?php echo $_SESSION['firstName'] . " " . $_SESSION['lastName']; ?>
             </h2>
@@ -133,8 +130,7 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
                         <img id="uploadedImageUser" src="../img/upload.png" alt="Uploaded Image" height="200">
                     </div>
                     <div>
-                        <input id="uploadedImageUser1" type="file" accept="image/*" onchange="loadImageUser(event)"
-                            required>
+                        <input id="uploadedImageUser1" type="file" accept="image/*" onchange="loadImageUser(event)" required>
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -150,8 +146,7 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
     </div>
 
     <!--Modal for USER EDIT ACCOUNT-->
-    <div class="modal fade" id="staticBackdropYow" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="staticBackdropYow" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content mt-5">
                 <div class="modal-header">
@@ -163,17 +158,14 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
                         <div id="imgUpl">
                             <label class="labelImage" for="uploadInput">Upload New Profile Picture</label>
                             <div class="image-holder d-grid gap-2 d-md-flex justify-content-md-center">
-                                <img id="uploadedImageCatalog" src="../img/upload.png" alt="Uploaded Image"
-                                    height="200">
+                                <img id="uploadedImageCatalog" src="../img/upload.png" alt="Uploaded Image" height="200">
                             </div>
-                            <input id="uploadInput" type="file" name="catalogImg" accept="image/*"
-                                onchange="catalogImgUp(event)">
+                            <input id="uploadInput" type="file" name="catalogImg" accept="image/*" onchange="catalogImgUp(event)">
                         </div>
 
                         <div class="form-floating mb-3 col-11 gx-2 gy-2 mx-auto">
                             <!-- Gap on all sides is 2 -->
-                            <input type="text" id="catalogTitle" name="catalogTitle" class="form-control"
-                                placeholder="Address">
+                            <input type="text" id="catalogTitle" name="catalogTitle" class="form-control" placeholder="Address">
                             <label id="catalogTitleLabel" for="companyName">Address</label>
                         </div>
 
@@ -194,14 +186,13 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
         <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel"><span>Username's</span> Profile</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel"><span class="text-primary"><?php echo $fullname; ?></span><span>'s</span> Dashboard</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div class="modal-body-view-more">
-                        <div class="modal-pic-container">
-                            <img id="userPic" src="../img/uploads/company/<?php echo $fetch['user_image']; ?>"
-                                alt="user profile" title="user profile">
+                        <!-- <div class="modal-pic-container">
+                            <img id="userPic" src="../img/uploads/company/<?php echo $fetch['user_image']; ?>" alt="user profile" title="user profile">
                         </div>
 
                         <div class="modal-name-container">
@@ -226,13 +217,14 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
                             <div class="freelanceEmail marg">
                                 <?php echo $fetch['email']; ?>
                             </div>
-                        </div>
+                        </div> -->
 
+                        <!-- DASHBOARD -->
                         <div>
-                            <div id="" class="titles">
+                            <div id="" class="titles text-primary">
                                 Dashboard:
                             </div>
-                            <div class="box-">
+                            <div class="box- justify-content-center">
                                 <div class="box-1 boxes">
                                     <span>Posted Jobs:</span>
                                     <span class="boxes-data" id="total_posts">100</span>
@@ -249,11 +241,130 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
                             </div>
                         </div>
 
+                        <!-- USER POSTS -->
+                        <div id="" class="titles mt-4 mb-2">
+                            <span class="text-primary titles">Posted Jobs:</span>
+                        </div>
+                        <div class="list-group">
+
+                            <button type="button" class="list-group-item list-group-item-action">
+                                <div class="d-flex">
+                                    <div class="p-2 flex-grow-1">Web Developer</div>
+                                    <div class="p-2 date-posted fw-light fst-italic">11-12-22</div>
+                                    <div class="p-2 border border-primary rounded ms-3" data-bs-toggle="modal" data-bs-target="#edit-post-modal">Edit</div>
+                                </div>
+                            </button>
+
+                            <button type="button" class="list-group-item list-group-item-action">
+                                <div class="d-flex">
+                                    <div class="p-2 flex-grow-1">Mobile Developer</div>
+                                    <div class="p-2 date-posted fw-light fst-italic">01-01-23</div>
+                                    <div class="p-2 border border-primary rounded ms-3" data-bs-toggle="modal" data-bs-target="#edit-post-modal">Edit</div>
+                                </div>
+                            </button>
+
+                            <button type="button" class="list-group-item list-group-item-action">
+                                <div class="d-flex">
+                                    <div class="p-2 flex-grow-1">Bootstrap Gods</div>
+                                    <div class="p-2 date-posted fw-light fst-italic">04-19-20</div>
+                                    <div class="p-2 border border-primary rounded ms-3" data-bs-toggle="modal" data-bs-target="#edit-post-modal">Edit</div>
+                                </div>
+                            </button>
+
+                        </div>
+
                     </div>
                 </div>
+                <div class=" modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- MODAL FOR EDIT POST MODAL-->
+    <div class="modal fade" id="edit-post-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+
+        <div class="modal-dialog modal-md modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Post</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="post_form" method="POST">
+                    <div class="toFlex d-flex justify-content-center p-3">
+                        <div class="dropdownOptionPost">
+                            <select id="filterOptionPost" name="post_category">
+                                <option value="Website Development">Website Development</option>
+                                <option value="Mobile Development">Mobile Development</option>
+                                <option value="Website Hosting">Website Hosting</option>
+                                <option value="Multimedia">Multimedia</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="mx-3 mb-3">
+                        <!-- <input type="text" id="title" name="post_title" placeholder="Job Title" required> -->
+                        <div class="input-group flex-nowrap">
+                            <span class="input-group-text" id="addon-wrapping">Job Title</span>
+                            <input type="text" class="form-control" placeholder="Edit job title" aria-label="Username" aria-describedby="addon-wrapping">
+                        </div>
+                    </div>
+
+                    <div class="descContainer mx-3 mb-3">
+                        <!-- <textarea id="description" placeholder="Job Description" name="post_description"></textarea> -->
+                        <div class="input-group">
+                            <span class="input-group-text">Description</span>
+                            <textarea class="form-control" aria-label="Edit description"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="mx-3 mb-3">
+                        <!-- <input type="text" id="tags" name="post_tags" placeholder="Tags" required> -->
+                        <div class="input-group flex-nowrap">
+                            <span class="input-group-text" id="addon-wrapping">Tag</span>
+                            <input type="text" class="form-control" placeholder="Edit tag" aria-label="Username" aria-describedby="addon-wrapping">
+                        </div>
+                    </div>
+
+                    <div class="mx-3 mb-3">
+                        <div class="input-group">
+                            <span class="input-group-text">&#8369;</span>
+                            <input type="number" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" name="rate" placeholder="Enter rate">
+                            <span class="input-group-text">0.00</span>
+                        </div>
+                    </div>
+
+                    <!-- <div class="btns">
+                            <input id="submitPost" class="btn" type="button" value="Submit"
+                                onclick="new Posts().post();">
+                            <input id="clearPost" class="btn" type="button" value="Clear">
+                        </div> -->
+                </form>
+
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" data-bs-toggle="modal"
-                        data-bs-target="#">Close</button>
+                    <!-- <button type="button" class="btn btn-danger"  data-bs-target="#delete-post-modal">Delete</button> -->
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-post-modal">Delete</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-post-modal">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- CONFIRMATION DELETE POST MODAL -->
+    <div class="modal fade" id="delete-post-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Post?</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete this post?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Back</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button>
                 </div>
             </div>
         </div>
@@ -291,14 +402,13 @@ $fullname = $fetch['firstName'] . ' ' . $fetch['lastName'];
     <script src="../classJS/Account.js"></script>
     <script src="../js/user.js"></script>
 
-    <script src="https://code.jquery.com/jquery-3.7.0.js"
-        integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
 
     <script>
-    new Dashboard().get_information_company();
+        new Dashboard().get_information_company();
     </script>
 </body>
 
