bb80531346ec688e6b7847aca16aa87be876be17
vinnny1904 <bokarvin1904.com>
2023-11-01 14:59:22 +0800

    modified

--- admin/admin.php
+++ admin/admin.php
@@ -23,7 +23,7 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><span class="white-"><img src="img/eGAWAwhite.png" alt="" style="height:40px;"></span></a>
+            <a class="navbar-brand" href="#"><span class="white-"><img src="../img/eGAWAwhite.png" alt="" style="height:40px;"></span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon "></span>
             </button>
