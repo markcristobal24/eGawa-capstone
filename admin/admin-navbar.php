abff81c5d3a88ebcb1666bb20615e972ae4d25d1
vinnny1904 <bokarvin1904.com>
2023-11-02 11:56:47 +0800

    modification on admin

---
+++ admin/admin-navbar.php
@@ -0,0 +1,59 @@
+<style>
+    /* ===for nav bar====== */
+    nav {
+        background-image: linear-gradient(to right, #0073aa, #8000aa);
+        box-shadow: 0px 0 10px 10px rgba(0, 0, 0, 0.5);
+        color: #fff;
+    }
+
+    .white- {
+        color: #fff;
+    }
+
+    .hov-:hover {
+        color: coral;
+    }
+</style>
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><span class="white-"><img src="../img/eGAWAwhite.png" alt="" style="height:40px;"></span></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon "></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#"><span class="white- hov-">Dashboard</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="white- hov-">Reports</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="white- hov-">Manage Users</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Freelancer</a></li>
+                        <li><a class="dropdown-item" href="#">Company</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Banned Users</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="d-flex">
+                <span class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="white- hov-">Hi, Admin <span>Sulit</span></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Log out</a></li>
+                    </ul>
+                </span>
+            </div>
+        </div>
+    </div>
+</nav>
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
 
     <!-- Link for CSS -->
-    <link rel="stylesheet" href="admin.css" />
+    <link rel="stylesheet" href="dashboard.css" />
     <link rel="stylesheet" href="css/notification.css">
 
     <!-- For social icons in the footer -->
@@ -21,47 +21,7 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><span class="white-"><img src="../img/eGAWAwhite.png" alt="" style="height:40px;"></span></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon "></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#"><span class="white- hov-">Dashboard</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><span class="white- hov-">Reports</span></a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="white- hov-">Manage Users</span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Freelancer</a></li>
-                            <li><a class="dropdown-item" href="#">Company</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Banned Users</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <div class="d-flex">
-                    <span class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="white- hov-">Hi, Admin <span>Sulit</span></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Log out</a></li>
-                        </ul>
-                    </span>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include "admin-navbar.php"; ?>
 
     <!-- <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#side-menu" aria-controls="offcanvasExample">
         Menu
@@ -81,6 +41,10 @@
                     <span>Subscribed:</span>
                     <span class="boxes-data">60</span>
                 </div>
+                <div class="box-3 boxes">
+                    <span>Banned:</span>
+                    <span class="boxes-data">60</span>
+                </div>
 
             </div>
 
@@ -94,9 +58,13 @@
                     <span>Subscribed:</span>
                     <span class="boxes-data">60</span>
                 </div>
+                <div class="box-3 boxes">
+                    <span>Banned:</span>
+                    <span class="boxes-data">60</span>
+                </div>
 
-                <div class="box-4 boxes"></div>
-                <div class="box-5 boxes"></div>
+                <!-- <div class="box-4 boxes"></div> -->
+                <!-- <div class="box-5 boxes"></div> -->
             </div>
         </div>
     </div>
