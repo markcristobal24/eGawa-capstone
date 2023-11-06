c16024fa7c8a70dfcc991805bbdd5f8e375d2479
Mark Cristobal <markjoshcristobal24@gmail.com>
2023-11-06 21:11:24 +0800

    added admin id verification

--- admin/freelance_verification.php
+++ admin/freelance_verification.php
@@ -1,3 +1,7 @@
+<?php
+require_once dirname(__FILE__) . "/../php/classes/DbClass.php";
+$db = new DbClass();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +13,7 @@
 
     <!-- Link for CSS -->
     <link rel="stylesheet" href="freelance_verification.css" />
-    <link rel="stylesheet" href="css/notification.css">
+    <link rel="stylesheet" href="../css/notification.css">
 
     <!-- For social icons in the footer -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -22,7 +26,7 @@
 <body>
 
     <?php include "admin-navbar.php"; ?>
-
+    <div class="toast_notif" id="toast_notif"></div>
     <div class="container- mt-5 pt-5">
 
         <div class="content">
@@ -34,19 +38,47 @@
                             <table class="table table-hover">
                                 <thead>
                                     <tr>
-                                        <th scope="col">#</th>
+                                        <th scope="col">ID</th>
                                         <th scope="col">Firstname</th>
                                         <th scope="col">Lastname</th>
                                         <th scope="col">Date</th>
-                                        <th scope="col">ID</th>
+                                        <th scope="col">ID Type</th>
+                                        <th scope="col">ID Number</th>
+                                        <th scope="col">Front ID</th>
+                                        <th scope="col">Back ID</th>
                                         <th scope="col" class="d-flex">
                                             <span>Action</span>
                                         </th>
                                     </tr>
                                 </thead>
                                 <tbody>
-
-                                    <tr data-bs-toggle="modal" data-bs-target="#report-user-modal">
+                                    <?php
+                                    $query = $db->connect()->prepare("SELECT * FROM id_verification INNER JOIN account ON id_verification.account_id = account.account_id WHERE verify_status = :status ORDER BY id_verification.id ASC");
+                                    $query->execute([':status' => 'PENDING']);
+                                    foreach ($query as $row) {
+                                        $id = $row['id'];
+                                        $currentDateTime = $row['timestamp'];
+                                        $dateTimeObj = new DateTime($currentDateTime);
+                                        $date_submitted = $dateTimeObj->format("m-d-Y");
+                                        echo '
+                                        <tr>
+                                            <th scope="row">' . $id . '</th>
+                                            <td>' . $row['firstName'] . '</td>
+                                            <td>' . $row['lastName'] . '</td>
+                                            <td>' . $date_submitted . '</td>
+                                            <td>' . $row['id_type'] . '</td>
+                                            <td>' . $row['id_number'] . '</td>
+                                            <td><a href="../img/uploads/freelancer/id/' . $row['front_image'] . '""><img src = "../img/uploads/freelancer/id/' . $row['front_image'] . '" style = "height: 100px;"></a></td>
+                                            <td><a href="../img/uploads/freelancer/id/' . $row['back_image'] . '""><img src = "../img/uploads/freelancer/id/' . $row['back_image'] . '" style = "height: 100px;"></a></td>
+                                            <td>
+                                                <button type="button" id="btn_acceptverify" class="btn btn-primary" onclick="new Admin().accept_idverify(' . $id . ');">Accept</button>
+                                                <button type="button" id="btn_denyverify" class="btn btn-secondary" onclick="new Admin().deny_idverify(' . $id . ');">Decline</button>
+                                            </td>
+                                        </tr>
+                                        ';
+                                    }
+                                    ?>
+                                    <!-- <tr data-bs-toggle="modal" data-bs-target="#report-user-modal">
                                         <th scope="row">1</th>
                                         <td>Arvin</td>
                                         <td>Bok</td>
@@ -92,7 +124,7 @@
                                             <button class="btn btn-primary">Accept</button>
                                             <button class="btn btn-secondary">Decline</button>
                                         </td>
-                                    </tr>
+                                    </tr> -->
 
                                 </tbody>
                             </table>
@@ -102,7 +134,9 @@
             </div>
         </div>
     </div>
-
+    <script src="../classJS/Account.js"></script>
+    <script src="../classJS/Notification.js"></script>
+    <script src="../classJS/Admin.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
--- classJS/Admin.js
+++ classJS/Admin.js
@@ -1,5 +1,7 @@
 class Admin {
     accept_idverify(id) {
+        let button_value = new Account().get_button_value("btn_acceptverify");
+        new Account().button_loading("btn_acceptverify", "loading", "");
         let form_data = new FormData();
         form_data.append('accept_id', 'accept_id');
         form_data.append('id', id);
@@ -19,12 +21,15 @@ class Admin {
                 }, 1000);
             }
             else if (response_data.error) {
+                new Account().button_loading("btn_acceptverify", "", button_value);
                 new Notification().create_notification(response_data.error, "error");
             }
         });
     }
 
     deny_idverify(id) {
+        let button_value = new Account().get_button_value("btn_denyverify");
+        new Account().button_loading("btn_denyverify", "loading", "");
         let form_data = new FormData();
         form_data.append('deny_id', 'deny_id');
         form_data.append('id', id);
@@ -44,6 +49,7 @@ class Admin {
                 }, 1000);
             }
             else if (response_data.error) {
+                new Account().button_loading("btn_denyverify", "", button_value);
                 new Notification().create_notification(response_data.error, "error");
             }
         });
