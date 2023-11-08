f2216a1dae722cda953db61ecb1a91d51162ef68
vinnny1904 <bokarvin1904.com>
2023-11-08 14:38:04 +0800

    modified

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,9 +19,7 @@ if ($_SESSION['userType'] !== "super_admin") {
     <link rel="stylesheet" href="css/notification.css">
 
     <!-- For social icons in the footer -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
@@ -36,10 +34,13 @@ if ($_SESSION['userType'] !== "super_admin") {
         Menu
     </button> -->
 
-    <div class="container- mt-5 pt-5">
+
+    <div class="container mt-5 pt-5">
+
+        <div class="p-3 mb-2 bg-secondary text-white fs-3">Dashboard</div>
 
         <div class="content">
-            <h1>Dashboard</h1>
+            <!-- <h1>Dashboard</h1> -->
             <span class="row-title titles">Users:</span>
             <div class="box-">
                 <div class="box-1 boxes">
@@ -102,8 +103,7 @@ if ($_SESSION['userType'] !== "super_admin") {
         </div>
     </div> -->
     <script src="../js/script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.js"
-        integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
