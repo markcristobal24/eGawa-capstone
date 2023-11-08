f6bc86090de408ef35fa3c899ed9f1337a9bff85
vinnny1904 <bokarvin1904.com>
2023-11-08 14:21:46 +0800

    modified colors

--- admin/logs.php
+++ admin/logs.php
@@ -25,6 +25,13 @@ if ($_SESSION['userType'] !== "super_admin") {
 
     <style>
         /* Custom CSS to change button colors */
+        .color- {
+            color: #808080;
+        }
+
+        .nav-link.active .text-success-emphasis {
+            color: white;
+        }
     </style>
 
     <title>eGawa | Admin Logs</title>
@@ -42,14 +49,15 @@ if ($_SESSION['userType'] !== "super_admin") {
             <div>
                 <ul class="nav nav-pills mb-3 " id="pills-tab" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true"><span class="text-success-emphasis">Freelance</span></button>
+                        <button class="nav-link active color-" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Freelance</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false"><span class="text-success-emphasis">Company</span></button>
+                        <button class="nav-link color-" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Company</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false"><span class="text-success-emphasis">Messages</span></button>
+                        <button class="nav-link color-" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Messages</button>
                     </li>
+                </ul>
                 </ul>
             </div>
             <div>
