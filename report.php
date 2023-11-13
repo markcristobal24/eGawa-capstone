0bdfed2ee1bc5bb20191ad980c8d2adcfc5383c1
vinnny1904 <bokarvin1904.com>
2023-11-13 21:07:30 +0800

    modified

--- report.php
+++ report.php
@@ -20,22 +20,24 @@ if ($query->rowCount() > 0) {
     $delay = 100;
 
     $html .= '<body style="width:100%;">';
-    $html .= '<div style="text-align: center;"><img src="/img/egawaicon4.png"></div>';
+    $html .= '<div style="text-align: center;"><img src="img/icon.jpg" width="50" height="50"></div>';
+    $html .= '<div style="text-align: center;"><span style="font-size: 19px; font-weight: bold;">eGAWA</span></div>';
+    $html .= '<div style="text-align: center;"><span style="font-size: 17px; font-weight: bold;">Online Freelance Services Platform</span></div>';
     $html .= '<div style="text-align: center;">Transaction Report</div>';
     $html .= '<br><br>';
-    $html .= '<div style="text-align:left;">Date Generated: ' . date('F d, Y', strtotime($currentDate)) . '</div>';
+    $html .= '<div style="text-align:left;"><span style="font-weight: bold;">Date Generated:</span> ' . date('F d, Y', strtotime($currentDate)) . '</div>';
     $html .= '<br>';
 
     $html .= '<table class="table" style="width:100%;">';
-    $html .= '<tr><td>Transaction ID</td> <td>Freelancer Name</td> <td>Company/Employer Name</td> <td>Date of Transaction</td> </tr>';
+    $html .= '<tr><td style="font-weight: bold;">Transaction ID</td> <td style="font-weight: bold;">Freelancer Name</td> <td style="font-weight: bold;">Company/Employer Name</td> <td style="font-weight: bold;">Date of Transaction</td> </tr>';
     while ($row = $query->fetch(PDO::FETCH_BOTH)) {
         $counter++;
-        $html .= '<tr><td>' . $row['application_id'] . '</td><td>' . $row['freelancer_name'] . '</td><td>' . $row['company_name'] . '</td><td>' . $row['timestamp'] . '</td></tr>';
+        $html .= '<tr><td style="text-align: center;"><span>' . $row['application_id'] . '</span></td><td style="text-align: center;">' . $row['freelancer_name'] . '</td><td style="text-align: center;">' . $row['company_name'] . '</td><td style="text-align: center;"><span>' . $row['timestamp'] . '</span></td></tr>';
     }
 
     $html .= '</table>';
     $html .= '<br><br>';
-    $html .= '<div style="text-align:right;">Total Transaction: ' . $counter . '</div>';
+    $html .= '<div style="text-align:right;"><span style="font-weight: bold;">Total Transaction:</span> ' . $counter . '</div>';
     $html .= '</body>';
 } else {
     $html = "No records found";
