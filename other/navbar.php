cb79c8e3091c6b316c43804f73ce3463fdf92025
vinnny1904 <bokarvin1904.com>
2023-11-19 14:41:46 +0800

    added hub

--- other/navbar.php
+++ other/navbar.php
@@ -3,75 +3,75 @@
         <?php
         if (isset($_SESSION['email']) && isset($_SESSION['userType']) && $_SESSION['userType'] == "freelancer") {
         ?>
-        <a class="navbar-brand" href="../freelance/freelanceHome.php"><img src="../img/eGAWAwhite.png" alt="Logo"
-                id="logoImage"></a>
+            <a class="navbar-brand" href="../freelance/freelanceHome.php"><img src="../img/eGAWAwhite.png" alt="Logo" id="logoImage"></a>
         <?php
         } else if (isset($_SESSION['email']) && isset($_SESSION['userType']) && $_SESSION['userType'] == "user") {
         ?>
-        <a class="navbar-brand" href="../user/userHome.php"><img src="../img/eGAWAwhite.png" alt="Logo"
-                id="logoImage"></a>
+            <a class="navbar-brand" href="../user/userHome.php"><img src="../img/eGAWAwhite.png" alt="Logo" id="logoImage"></a>
         <?php
         }
         ?>
         <!-- <a class="navbar-brand" href="../user/userHome.php"><img src="../img/eGAWAwhite.png" alt="Logo" -->
         <!-- id="logoImage"></a> -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <?php if (isset($_SESSION['email']) && isset($_SESSION['userType']) && $_SESSION['userType'] == "freelancer") : ?>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a id="home1" class="nav-link" href="../freelance/freelanceHome.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a id="about1" id="about" class="nav-link" href="../other/about_egawa.php">About</a>
-                </li>
-                <li class="nav-item">
-                    <a id="freeLanceInbox" class="nav-link" href="freelance_message.php">Messages </a>
-                </li>
-                <li class="nav-item dropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a id="home1" class="nav-link" href="../freelance/freelanceHome.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="about1" id="about" class="nav-link" href="../other/about_egawa.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="freeLanceInbox" class="nav-link" href="freelance_message.php">Messages </a>
+                    </li>
+                    <li class="nav-item dropdown">
 
-                    <a id="freelanceOption" class="nav-link" href="#">Welcome,
-                        <span id="welcomeName">
-                            <?php echo $_SESSION['firstName']; ?>
-                        </span></a>
-                    <div class="dropdown-content">
-                        <a href="../freelance/freelanceHomePage.php">My Profile</a>
-                        <a href="../change_email.php">Change Email Address</a>
-                        <a href="../change_password.php">Change Password</a>
-                        <a id="logout1" href="#">Log Out</a>
-                    </div>
+                        <a id="freelanceOption" class="nav-link" href="#">Welcome,
+                            <span id="welcomeName">
+                                <?php echo $_SESSION['firstName']; ?>
+                            </span></a>
+                        <div class="dropdown-content">
+                            <a href="../freelance/freelanceHomePage.php">My Profile</a>
+                            <a href="../change_email.php">Change Email Address</a>
+                            <a href="../change_password.php">Change Password</a>
+                            <a id="logout1" href="#">Log Out</a>
+                        </div>
 
-                </li>
-            </ul>
+                    </li>
+                </ul>
             <?php elseif (isset($_SESSION['email']) && isset($_SESSION['userType']) && $_SESSION['userType'] == "user") : ?>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a id="home1" class="nav-link" href="../user/userHome.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a id="about1" id="about" class="nav-link" href="../other/about_egawa.php">About</a>
-                </li>
-                <li class="nav-item">
-                    <a id="freeLanceInbox" class="nav-link" href="user_message.php">Messages </a>
-                </li>
-                <li class="nav-item dropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a id="home1" class="nav-link" href="../user/userHome.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="home1" class="nav-link" href="../user/freelancers_hub.php">Hub</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="about1" id="about" class="nav-link" href="../other/about_egawa.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="freeLanceInbox" class="nav-link" href="user_message.php">Messages </a>
+                    </li>
+                    <li class="nav-item dropdown">
 
-                    <a id="freelanceOption" class="nav-link" href="#">Welcome,
-                        <span id="welcomeName">
-                            <?php echo $_SESSION['firstName']; ?>
-                        </span></a>
-                    <div class="dropdown-content">
-                        <a href="../user/userHomePage.php">My Profile</a>
-                        <a href="../user/userEditProfile.php">Edit Profile</a>
-                        <a href="../change_email.php">Change Email Address</a>
-                        <a href="../change_password.php">Change Password</a>
-                        <a id="logout1" href="#">Log Out</a>
-                    </div>
-                </li>
-            </ul>
+                        <a id="freelanceOption" class="nav-link" href="#">Welcome,
+                            <span id="welcomeName">
+                                <?php echo $_SESSION['firstName']; ?>
+                            </span></a>
+                        <div class="dropdown-content">
+                            <a href="../user/userHomePage.php">My Profile</a>
+                            <a href="../user/userEditProfile.php">Edit Profile</a>
+                            <a href="../change_email.php">Change Email Address</a>
+                            <a href="../change_password.php">Change Password</a>
+                            <a id="logout1" href="#">Log Out</a>
+                        </div>
+                    </li>
+                </ul>
             <?php endif; ?>
         </div>
     </div>
